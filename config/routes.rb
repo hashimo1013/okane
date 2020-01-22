11226cc4c81a016fdf1d8efe481b097ccd305fa6
hashimo1013 <hashimoto.hazimeta1013@gmail.com>
2020-01-22 16:01:15 +0900

    plus action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "money#index"
   devise_for :users
-  resources :money
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :money do
+    collection do
+      get 'file'
+    end
+  end
 end
