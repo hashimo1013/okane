98257f9aace601a6b1528c0ec03f04dc82008342
hashimo1013 <hashimoto.hazimeta1013@gmail.com>
2020-02-23 13:09:48 +0900

    cmseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+[
+  '食費','日用品','交際費','趣味','雑費'
+].each do |tag|
+  Tag.create!(
+    { tag: tag}
+  )
+end
