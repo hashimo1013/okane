64c1ecc2e9778fc0c30a1a69d9854899a5af39a6
hashimo1013 <hashimoto.hazimeta1013@gmail.com>
2020-03-06 02:32:48 +0900

    chage-preview

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,15 +1,18 @@
 $(function(){
   // プレビュー用のimgタグを生成
   const buildImg = (url)=> {
-    const html = `<img src="${url}" width="600px" height="600px">`;
+    const html = `<img src="${url}" width="600px" height="600px" id=previewImage >`;
     return html;
   }
 // 画像を選択
   $('#money_image').change(function(e){
-    console.log(333);
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
-    $('#previews').append(buildImg(blobUrl));
+    if ($('#previewImage').length) {
+      $('#previewImage').attr("src", blobUrl);
+    } else {
+    $('#preview').append(buildImg(blobUrl));
+    }
   })
 
 })
